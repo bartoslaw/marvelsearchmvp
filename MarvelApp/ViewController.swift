660055a5d6fa7f1c4224f253193a024b9ce0fb56
Bartłomiej Smektała <bartlomiejsmektala@MacBook-Pro-Bartomiej.local>
2020-01-09 15:48:38 +0100

    Init commit

---
+++ MarvelApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MarvelApp
+//
+//  Created by Bartłomiej Smektała on 09/01/2020.
+//  Copyright © 2020 Bartłomiej Smektała. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
